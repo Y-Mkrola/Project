dd8686e214c74c6eb33082026de76922997592d2
Y-Mkrola <150502576+Y-Mkrola@users.noreply.github.com>
2023-12-07 17:29:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function displayTemperature(response) {
     let hours = date.getHours();
     let day = date.getDay();
   
-    if (minutes < 9) {
+    if (minutes < 10) {
       minutes = `0${minutes}`;
     }
   
