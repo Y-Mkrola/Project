bc7e8149321c0acd7d0d0f146facffef4975664d
Y-Mkrola <150502576+Y-Mkrola@users.noreply.github.com>
2023-12-09 21:21:48 +0200

    edited js an html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 function displayTemperature(response) {
-    let temperatureElement = document.querySelector("#current-temperature");
-    let temperature = Math.round(response.data.temperature.current);
-    let cityElement = document.querySelector("#current-city");
-    let descriptionElement = document.querySelector("#description");
-    cityElement.innerHTML = response.data.city;
-    descriptionElement.innerHTML=response.data.condtion.description;
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let weatherIconElement = document.querySelector("#weather-icon");
 
-    temperatureElement.innerHTML = temperature;
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.humidity;
+  windSpeedElement.innerHTML = response.data.wind.speed;
+
+  temperatureElement.innerHTML = temperature;
+
+
+  setWeatherIcon(weatherIconElement, response.data.condition.icon);
 
     console.log(response.data);
   }
+  function setWeatherIcon(element, iconCode) {
+  }
+    const iconMappings = {
+        "01d": "☀️", // clear sky day
+        "01n": "🌙", // clear sky night
+        "02d": "⛅", // few clouds day
+        "02n": "🌥️", // few clouds night
+        "03d": "☁️", // scattered clouds day
+        "03n": "☁️", // scattered clouds night
+        "04d": "☁️", // broken clouds day
+        "04n": "☁️", // broken clouds night
+        "09d": "🌧️", // shower rain day
+        "09n": "🌧️", // shower rain night
+        "10d": "🌦️", // rain day
+        "10n": "🌦️", // rain night
+        "11d": "⛈️", // thunderstorm day
+        "11n": "⛈️", // thunderstorm night
+        "13d": "❄️", // snow day
+        "13n": "❄️", // snow night
+        "50d": "🌫️", // mist day
+        "50n": "🌫️", // mist night
+    };
 
   function search(event) {
     event.preventDefault();
